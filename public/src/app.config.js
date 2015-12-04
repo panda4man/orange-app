dc86a5dab8bc39654bf029f80f3c643b09b53d2b
Andrew Clinton <ddrew.clinton@gmail.com>
2015-12-04 00:09:03 -0500

    added two new abstract states to establish two dif layouts

--- public/src/app.config.js
+++ public/src/app.config.js
@@ -14,22 +14,40 @@
             .state('app', {
                 url: '',
                 abstract: true,
-                templateUrl: '/cache/base.html',
+                template: '<div ui-view></div>',
                 controller: 'BaseCtrl as Base'
             })
-            .state('app.chat', {
+            /**
+             * This state is used for views requiring a header and footer.
+             * @type {String}
+             */
+            .state('app.master', {
+                url: '',
+                templateUrl: '/cache/layouts/master.html',
+                abstract: true
+            })
+            /**
+             * This state is used for views which only need a modal style such as login.
+             * @type {String}
+             */
+            .state('app.simple', {
+                url: '',
+                templateUrl: '/cache/layouts/simple.html',
+                abstract: true
+            })
+            .state('app.master.chat', {
                 url: '/chat',
                 title: 'Chat',
                 controller: 'ChatCtrl as Base',
                 templateUrl: '/cache/chat.html'
             })
-            .state('app.profile', {
+            .state('app.master.profile', {
                 url: '/profile',
                 title: 'Profile',
                 controller: 'ProfileCtrl as Profile',
                 templateUrl: '/cache/profile/index.html'
             })
-            .state('app.profile-edit', {
+            .state('app.master.profile-edit', {
                 url: '/profile/edit',
                 title: 'Profile | Edit',
                 controller: 'ProfileCtrl as Profile',
