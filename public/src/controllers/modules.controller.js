de654681b6f135c4ae00bf7551a8cd0bb00f9b69
Andrew Clinton <ddrew.clinton@gmail.com>
2015-12-04 00:23:34 -0500

    added the new module

--- public/src/controllers/modules.controller.js
+++ public/src/controllers/modules.controller.js
@@ -1,4 +1,4 @@
 (function() {
     'use strict';
-    angular.module('orange.controllers', ['orange.controller.base', 'orange.controller.chat', 'orange.controller.profile']);
+    angular.module('orange.controllers', ['orange.controller.base', 'orange.controller.chat', 'orange.controller.profile', 'orange.controller.register']);
 })();
