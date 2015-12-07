8f4c8c020af05a97b2a1c8c4b167db8fb980c59e
Andrew Clinton <ddrew.clinton@gmail.com>
2015-12-07 14:37:12 -0500

    add new controller modules

--- public/src/controllers/modules.controller.js
+++ public/src/controllers/modules.controller.js
@@ -1,4 +1,4 @@
 (function() {
     'use strict';
-    angular.module('orange.controllers', ['orange.controller.base', 'orange.controller.hangman', 'orange.controller.profile', 'orange.controller.register']);
+    angular.module('orange.controllers', ['orange.controller.base', 'orange.controller.hangman.base', 'orange.controllers.hangman', 'orange.controller.profile', 'orange.controller.register']);
 })();
