08195b202d7a842a04f68049f7e8ea3b75ef8ce6
Andrew Clinton <ddrew.clinton@gmail.com>
2015-12-07 16:24:15 -0500

    single point to update game list and added new listeners

--- public/src/controllers/hangman/hangman.index.controller.js
+++ public/src/controllers/hangman/hangman.index.controller.js
@@ -17,11 +17,26 @@
                 games: []
             };
 
+            getGames();
+        }
+
+        function getGames() {
+            console.log('Retrieving updated game list.');
             HangmanFactory.all().then(function(games) {
                 vm.data.games = games;
             }, function() {
 
             });
         }
+
+        //When a user leaves we need to reload the
+        $scope.socket.on('game:leave', function(player) {
+            getGames();
+        });
+
+        $scope.socket.on('game:ended', function () {
+            console.log('Game just closed');
+            getGames();
+        });
     }
 })();
