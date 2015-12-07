1d494c092f76ae0d57df6ecc41c272c29e1f696a
Andrew Clinton <ddrew.clinton@gmail.com>
2015-12-07 16:24:25 -0500

    more listeners

--- public/src/controllers/hangman/hangman.lobby.controller.js
+++ public/src/controllers/hangman/hangman.lobby.controller.js
@@ -15,12 +15,23 @@
     	function init () {
             console.log('Loading the hangman lobby controller');
     		vm.data = {
-
-    		};
+                game: null
+            };
 
             $scope.socket.emit('join', $scope.session.current_user, $stateParams.room);
     	}
 
+        $scope.socket.on('game:joined', function (data){
+            console.log('%s just joined the game!', data.player.full_name);
+            vm.data.game = data.game;
+        });
+
+        $scope.socket.on('game:leave', function (data){
+            console.log('%s just left the game!', data.player.full_name);
+            vm.data.game = data.game;
+        });
+
+        //If player leaves the lobby remove them from game.
         $scope.$on('$destroy', function () {
             console.log('%s is leaving the lobby', $scope.session.current_user.id);
             $scope.socket.emit('leave', $scope.session.current_user, $stateParams.room);
