13616cf09cbaa37aa24fd92e00e8cddde3e649f2
Andrew Clinton <ddrew.clinton@gmail.com>
2015-12-07 14:35:04 -0500

    added some logic to model methods

--- app/models/hangman.js
+++ app/models/hangman.js
@@ -5,30 +5,33 @@ var mongoose = require('mongoose'),
     uniqueValidator = require('mongoose-unique-validator');
 
 var GameSchema = new Schema({
-	owner: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true
-	},
-	status: {
-		required: true,
-		type: String,
-		enum: ['created', 'in_progress', 'done'],
-		index: true
-	},
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        required: true,
+        type: String,
+        enum: ['created', 'in_progress', 'done'],
+        index: true
+    },
     room: {
-    	required: true,
-    	index: true,
-    	type: String,
-    	unique: true
+        required: true,
+        index: true,
+        type: String,
+        unique: true
     },
     player_limit: {
-    	min: 0,
-    	max: 10,
-    	required: true,
-    	type: Number
-    }, 
-    players: [{type:mongoose.Schema.Types.ObjectId, ref: 'User'}]
+        min: 0,
+        max: 10,
+        required: true,
+        type: Number
+    },
+    players: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
 }, {
     toObject: {
         virtuals: true
@@ -38,23 +41,29 @@ var GameSchema = new Schema({
     }
 });
 
-GameSchema.methods.fill = function (data){
-	this.owner = mongoose.Types.ObjectId(data.owner);
-	this.room = data.room;
-	this.status = data.status;
-	this.player_limit = data.player_limit;
-	this.player = data.players;
+GameSchema.methods.fill = function(data) {
+    this.owner = mongoose.Types.ObjectId(data.owner);
+    this.room = data.room;
+    this.status = data.status;
+    this.player_limit = data.player_limit;
+    if(data.players && data.players.length){
+        console.log('saving players in fill method');
+        this.players = data.players;
+    }
 };
 
-GameSchema.methods.addPlayer = function (player){
-	this.players.push(mongoose.Types.ObjectId(player.id));
+GameSchema.methods.addPlayer = function(player) {
+    if (typeof player == "object")
+        this.players.push(mongoose.Types.ObjectId(player.id));
+    else
+        this.players.push(mongoose.Types.ObjectId(player));
 }
 
-GameSchema.methods.removePlayer = function (player){
-	var i = this.players.indexOf(player.id);
-	if(i != -1){
-		this.players.splice(i, 1);
-	}
+GameSchema.methods.removePlayer = function(player) {
+    var i = this.players.indexOf(player.id);
+    if (i != -1) {
+        this.players.splice(i, 1);
+    }
 }
 
 /**
