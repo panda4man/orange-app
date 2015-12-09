e4b28bf5987dfaed9bb29247105cea46a3e246b9
Andrew Clinton <ddrew.clinton@gmail.com>
2015-12-08 22:28:28 -0500

    implement new socket auth on client side

--- public/src/controllers/hangman.controller.js
+++ public/src/controllers/hangman.controller.js
@@ -5,26 +5,46 @@
         .module('orange.controller.hangman.base', [])
         .controller('HangmanCtrl', Controller);
 
-    Controller.$inject = ['$scope', '$state', '$timeout', 'SocketFactory']
+    Controller.$inject = ['$scope', '$rootScope', '$state', '$timeout', 'SocketFactory']
 
-    function Controller($scope, $state, $timeout, socket) {
+    function Controller($scope, $rootScope, $state, $timeout, socket) {
         var vm = this;
         init();
+
         function init() {
-            console.log('Loading the chat controller');
+            console.log('Loading the base hangman controller');
             $scope.socket = socket.hangman();
-            
-            $scope.socket.on('game:error', function (error){
+
+            //Listen to the server connect event
+            $scope.socket.on('connect', function () {
+                $scope.socket.emit('authenticate', {token: localStorage.getItem('satellizer_token')});
+            });
+
+            //Socket was not authorized so we need to relog
+            $scope.socket.on('unauthorized', function () {
+                $rootScope.$broadcast('event:auth-loginRequired');
+            });
+
+            $scope.socket.on('game:error', function(error) {
                 console.log(error);
             });
 
-            $scope.socket.on('game:joined', function (data){
+            $scope.socket.on('game:joined', function(data) {
                 console.log('Someone joined: ' + JSON.stringify(data));
             });
 
-            $scope.socket.on('game:left', function (data){
+            $scope.socket.on('game:left', function(data) {
                 console.log('Someone left ' + JSON.stringify(data));
-            })
+            });
+
+            $scope.socket.on("error", function(error) {
+                if (error.type == "UnauthorizedError" || error.code == "invalid_token") {
+                    // redirect user to login page perhaps?
+                    console.log("User's token has expired");
+                } else {
+                    console.log(error);
+                }
+            });
         }
     }
 })();
