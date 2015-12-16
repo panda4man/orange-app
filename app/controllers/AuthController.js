7bb1cf8d34511f9215cc697b808945184b2a5ef9
Andrew Clinton <ddrew.clinton@gmail.com>
2015-12-16 00:29:52 -0500

    updated auth controller

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,65 +1,69 @@
-var express = require('express');
-var User = require('../models/').user;
-var auth_middle = require('../middleware/auth');
-var jwt = require('../helpers/jwt');
-var router = express.Router();
+(function() {
+    'use strict';
+    var express = require('express'),
+        User = require('../models/').user,
+        auth_middle = require('../middleware/auth'),
+        jwt = require('../helpers/jwt'),
+        router = express.Router();
 
-router.post('/', function(req, res) {
-    User.findOne({
-        username: req.body.username
-    }, function(err, user) {
-        if (err) return res.status(400).json({
-            success: false,
-            error: 'Could not find user.'
-        });
-
-        if (!user) {
-            res.status(400).json({
+    router.post('/', function(req, res) {
+        User.findOne({
+            username: req.body.username
+        }, function(err, user) {
+            if (err) return res.status(400).json({
                 success: false,
-                message: 'Authentication failed. User not found.'
+                error: 'Could not find user.'
             });
-        } else if (user) {
-            // check if password matches
-            user.comparePassword(req.body.password, function(err, isMatch) {
-                if (err) return res.status(401).json(err);
 
-                if (!isMatch) return res.status(401).json({
+            if (!user) {
+                res.status(400).json({
                     success: false,
-                    error: 'Invalid credentials.'
+                    message: 'Authentication failed. User not found.'
                 });
-
-                var token = jwt.createToken(user);
-
-                User.findOne({
-                    'username': req.body.username
-                }, function(err, user) {
+            } else if (user) {
+                // check if password matches
+                user.comparePassword(req.body.password, function(err, isMatch) {
                     if (err) return res.status(401).json(err);
 
-                    if (!user) {
-                        res.status(401).json({
-                            success: false,
-                            error: 'Could not find user.'
-                        });
-                    } else {
-                        // return the information including token as JSON
-                        res.status(200).json({
-                            success: true,
-                            token: token,
-                            user: user
-                        });
-                    }
+                    if (!isMatch) return res.status(401).json({
+                        success: false,
+                        error: 'Invalid credentials.'
+                    });
+
+                    var token = jwt.createToken(user);
+
+                    User.findOne({
+                        'username': req.body.username
+                    }, function(err, user) {
+                        if (err) return res.status(401).json(err);
+
+                        if (!user) {
+                            res.status(401).json({
+                                success: false,
+                                error: 'Could not find user.'
+                            });
+                        } else {
+                            // return the information including token as JSON
+                            res.status(200).json({
+                                success: true,
+                                token: token,
+                                user: user
+                            });
+                        }
+                    });
                 });
-            });
-        }
-    })
-});
+            }
+        });
+    });
 
-router.post('/refresh', auth_middle, function(req, res) {
+    router.post('/refresh', auth_middle, function(req, res) {
+        console.log(req, res);
+    });
 
-});
+    router.post('/register', function(req, res) {
+        console.log(req, res);
+    });
 
-router.post('/register', function(req, res) {
+    module.exports = router;
 
-});
-
-module.exports = router;
+})();
